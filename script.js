b1d6848b7970370576a8d6580a43727b218d39c3
Állef G <allefgoncalves@gmail.com>
2023-04-26 18:22:29 -0300

    seleção

--- script.js
+++ script.js
@@ -6,20 +6,37 @@ usuario.innerHTML = `Olá, <strong>${user}</strong>!`;
 
 
 
-let modelo, gola, tecido;
+let modelo = null;
+let gola = null;
+let tecido = null;
 
 //ClearSelecao();
 
 
 
-function seleciona(x, classe){
-    console.log(x);
+function seleciona(escolhido, classe){
     let selecao = document.querySelector(`${classe} .selecionado`);
-    console.log(selecao);
     if(selecao != null){    //verificação de ja tem algum selecionado
         selecao.classList.remove(`selecionado`);
     }
-    x.querySelector('.imagem-tipo').classList.add('selecionado');
+    escolhido.querySelector('.imagem-tipo').classList.add('selecionado');
+    console.log(classe);
+
+    
+    if(classe == '.modelo')
+        modelo = escolhido.querySelector('.check').innerHTML;
+    
+    if(classe == '.gola')
+        gola = escolhido.querySelector('.check').innerHTML;
+    
+    if(classe == '.tecido')
+        tecido = escolhido.querySelector('.check').innerHTML;
+
+    console.log(escolhido);
+    console.log(modelo);
+    console.log(gola);
+    console.log(tecido);
+
 }
  /*   verifica(); //verificação se todos estão selecionados
 }
