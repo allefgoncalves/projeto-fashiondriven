0e8cd82f1c2108764d9ecfd68749b5206b8638be
Állef G <allefgoncalves@gmail.com>
2023-04-26 19:14:31 -0300

    seleção clear

--- script.js
+++ script.js
@@ -10,19 +10,20 @@ let modelo = null;
 let gola = null;
 let tecido = null;
 
-//ClearSelecao();
 
 
+console.log(modelo);
+console.log(gola);
+console.log(tecido);
 
 function seleciona(escolhido, classe){
     let selecao = document.querySelector(`${classe} .selecionado`);
-    if(selecao != null){    //verificação de ja tem algum selecionado
-        selecao.classList.remove(`selecionado`);
-    }
-    escolhido.querySelector('.imagem-tipo').classList.add('selecionado');
-    console.log(classe);
 
-    
+    if(selecao != null)   //verificação de ja tem algum selecionado
+        selecao.classList.remove(`selecionado`);
+
+    escolhido.querySelector('.imagem-tipo').classList.add('selecionado');
+        
     if(classe == '.modelo')
         modelo = escolhido.querySelector('.check').innerHTML;
     
@@ -32,31 +33,40 @@ function seleciona(escolhido, classe){
     if(classe == '.tecido')
         tecido = escolhido.querySelector('.check').innerHTML;
 
-    console.log(escolhido);
-    console.log(modelo);
-    console.log(gola);
-    console.log(tecido);
+    verifica(); //verificação se todos estão selecionados
+}
 
-}
- /*   verifica(); //verificação se todos estão selecionados
-}
 function verifica(){
-    if(modelo != ""){ //verifica se todos estão selecionados
-        if(gola != ""){
-            if(tecido != ""){ 
-                if(){   //se sim => verifica a url da imagem 
+    if(modelo != null){ //verifica se todos estão selecionados
+        if(gola != null){
+            if(tecido != null){ 
+                /*if(){   //se sim => verifica a url da imagem 
                     ClearSelecao(); //url ok => limpa a seleção
                     //libera o botão de finalizar o pedido
                 //url ruim => mensagem de erro 
-                }
+                }*/
             }
         }
     }
 }
 
 function ClearSelecao(){ //desmarca todos os itens 
-    modelo = "";
-    gola = "";
-    tecido = "";
+    if(modelo != null){
+        modelo = null;
+        let x = document.querySelector(`.modelo .selecionado`);
+        x.classList.remove(`selecionado`);
+    }
+ 
+    if(gola != null){
+       gola = null;
+        let y = document.querySelector(`.gola .selecionado`);
+        y.classList.remove(`selecionado`);
+    }
+
+    if(tecido != null){
+        tecido = null;
+        let z = document.querySelector(`.tecido .selecionado`);
+        z.classList.remove(`selecionado`);
+    }
     // limpar a URL
-}*/
+}
