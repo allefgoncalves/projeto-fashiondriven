de5bbd2539ef1745ab9cc79a3a050567b781920f
Állef G <allefgoncalves@gmail.com>
2023-04-27 11:39:11 -0300

    teste de serv

--- script.js
+++ script.js
@@ -8,17 +8,12 @@ let modelo = null;
 let gola = null;
 let tecido = null;
 let imagemReferencia = document.querySelector(`.img-ref`).value;
-
-
+let Pmodelo, Pgola, Ptecido;
  
 let imagemReferenciaInput = document.querySelector(`.img-ref`);
 imagemReferenciaInput.addEventListener("input", () =>{
     verifica();
 })
-
-console.log("teste2");
-console.log(imagemReferencia);
-console.log("teste3");
 
 function seleciona(escolhido, classe){
     let selecao = document.querySelector(`${classe} .selecionado`);
@@ -28,15 +23,35 @@ function seleciona(escolhido, classe){
 
     escolhido.querySelector('.imagem-tipo').classList.add('selecionado');
         
-    if(classe == '.modelo')
-        modelo = escolhido.querySelector('.check').innerHTML;
-    
-    if(classe == '.gola')
-        gola = escolhido.querySelector('.check').innerHTML;
-    
-    if(classe == '.tecido')
-        tecido = escolhido.querySelector('.check').innerHTML;
+    if(classe == '.modelo'){
+        Pmodelo = escolhido.querySelector('.check').innerHTML;
+        if(Pmodelo == 'camiseta')
+            modelo = 'top-tank';
+        if(Pmodelo == 'manga longa')
+            modelo = 'top-tank';
+        if(Pmodelo == 'T-shirt')
+            modelo = 't-shirt';
+    }
 
+    if(classe == '.gola'){
+        Pgola = escolhido.querySelector('.check').innerHTML;
+        if(Pgola == 'gola redonda')
+            gola = "round";
+        if(Pgola =='gola v')
+            gola = "v-neck";
+        if(Pgola =='gola polo' )
+            gola = "polo";   
+    }
+
+    if(classe == '.tecido'){
+        Ptecido = escolhido.querySelector('.check').innerHTML;
+        if(Ptecido =='seda')
+            tecido='silk';
+        if(Ptecido =='Algodão')
+            tecido='cotton';
+        if(Ptecido =='Poliester')
+            tecido='polyester';
+    }
     verifica(); //verificação se todos estão selecionados
 }
 
@@ -59,11 +74,65 @@ function verifica(){
                     botãofinal.classList.remove(`ativo`);
                     botãofinal.classList.add('inativo');
                     botãofinal.disabled = true;
-
                 }
             }
         }
     }
+}
+
+function finalizado(erro){
+    if (erro.response.status === 422){
+        alert(erro.response.value);
+        console.log("erro 442");
+    }
+    if(erro.response.status === 211){
+        console.log("status 211");
+        let mensagem = document.querySelector(`.MensagemFinal`);
+        mensagem.innerHTML = 
+        `<div class= 'elemento'>
+            <h4>Pedido realizado com sucesso!</h4>
+            <div>
+                <img src='${imagemReferencia}'>
+            </div>
+            <h5>voltando para a pagina principal em 10s<h5>
+        </div>
+        `;
+        mensagem.classList.remove('invisivel');
+
+        ClearSelecao();
+    }
+    console.log("erro numeral");
+}
+
+function ErroNoServ(){
+    console.log("erro");
+}
+
+function EnviarDados(click){
+    console.log("teste enviados");
+    const uri = imagemReferencia;
+    const encoded = encodeURI(uri);
+
+    let dado = {
+   "model": modelo,
+    "neck": gola,
+    "material": tecido,
+    "image":encoded,
+    "owner": user,
+    "author": user
+    } 
+    console.log(dado);
+    const promise = axios.post('https://mock-api.driven.com.br/api/v4/shirts-api/shirts',dado);
+    promise.then(finalizado);
+    promise.catch(ErroNoServ);
+    
+}
+
+function renderizaritens(){
+
+    const dado = document.querySelector('criados');
+    dado.innerHTML = ''
+
 }
 
 function ClearSelecao(){ //desmarca todos os itens 
@@ -85,4 +154,5 @@ function ClearSelecao(){ //desmarca todos os itens
         z.classList.remove(`selecionado`);
     }
     // limpar a URL
+    imagemReferencia = "";
 }
